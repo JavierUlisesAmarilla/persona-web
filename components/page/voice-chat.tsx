40b7fbfdb8119faad1009ca7194670e709b6969b
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-09-07 21:45:32 -0500

    use client component for voice chat

--- components/page/voice-chat.tsx
+++ components/page/voice-chat.tsx
@@ -1,4 +1,11 @@
-export default async function VoiceChat() {
+"use client"
+import { useEffect } from 'react';
+
+export default function VoiceChat() {
+  useEffect(() => {
+    console.log('VoiceChat#useEffect')
+  }, [])
+
   return (
     <div className="z-10">VoiceChat</div>
   );

f426fa4deab1609fd2728284185e1d8a3eeda000
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-09-07 21:09:52 -0500

    voice chat template

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Home from '@/components/page/home'
+import VoiceChat from '@/components/page/voice-chat'
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Page() {
-  return await Home()
+  const session = await getServerSession(authOptions);
+  return !session ? await VoiceChat() : await Home()
 }
---
+++ components/page/voice-chat.tsx
@@ -0,0 +1,5 @@
+export default async function VoiceChat() {
+  return (
+    <div className="z-10">VoiceChat</div>
+  );
+}

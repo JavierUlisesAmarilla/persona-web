0b8eda90644428e947f64f0a2ecf4efc6066f27c
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-09-10 23:43:59 -0500

    functionalize

--- components/page/voice-chat.tsx
+++ components/page/voice-chat.tsx
@@ -30,6 +30,92 @@ export default function VoiceChat() {
   const actionsSchemaTextRef = useRef<any>()
   const currentStateTextRef = useRef<any>()
 
+  const onUser = () => {
+    if (!personaClient || !userInputRef.current.value) {
+      return
+    }
+
+    personaClient.sendUserText(userInputRef.current.value)
+  }
+
+  const onAssistant = () => {
+    if (!personaClient || !assistantInputRef.current.value) {
+      return
+    }
+
+    personaClient.sayText(assistantInputRef.current.value)
+  }
+
+  const onInitialization = async () => {
+    const initialMsg = initialInputRef.current.value
+    const selPersonaId = personaSelRef.current.value
+
+    if (!personaClient || !initialMsg || !selPersonaId) {
+      return
+    }
+
+    setInitialMsgState('Saving...')
+    const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/initialMessage?apikey=${API_KEY}`, { initialMessage: initialMsg });
+
+    if (res.status === 200) {
+      setInitialMsgState('Success')
+    } else {
+      setInitialMsgState('Error')
+    }
+  }
+
+  const onRateLimit = async () => {
+    const rateLimitMsg = rateLimitInputRef.current.value
+    const selPersonaId = personaSelRef.current.value
+
+    if (!personaClient || !rateLimitMsg || !selPersonaId) {
+      return
+    }
+
+    setRateLimitMsgState('Saving...')
+    const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/rateLimitMessage?apikey=${API_KEY}`, { rateLimitMessage: rateLimitMsg });
+
+    if (res.status === 200) {
+      setRateLimitMsgState('Success')
+    } else {
+      setRateLimitMsgState('Error')
+    }
+  }
+
+  const onPrompt = async () => {
+    const promptText = promptTextRef.current.value
+    const selPersonaId = personaSelRef.current.value
+
+    if (!personaClient || !promptText || !selPersonaId) {
+      return
+    }
+
+    setPromptState('Saving...')
+    const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/prompt?apikey=${API_KEY}`, { prompt: promptText });
+
+    if (res.status === 200) {
+      setPromptState('Success')
+    } else {
+      setPromptState('Error')
+    }
+  }
+
+  const onNewChat = async () => {
+    const selPersonaId = personaSelRef.current.value
+
+    if (!personaArr.length || !selPersonaId) {
+      return
+    }
+
+    const selPersonName = personaArr.find((persona) => persona._id === selPersonaId)?.name
+
+    if (!personaClient || !selPersonName) {
+      return
+    }
+
+    await personaClient.init('admin', selPersonName)
+  }
+
   useEffect(() => {
     if (!isFirstRender) {
       return
@@ -59,84 +145,64 @@ export default function VoiceChat() {
     <div className="z-10 flex flex-col w-full gap-8 px-8">
       <div className='flex flex-col w-full gap-4'>
         <div className='flex items-center w-full gap-4'>
-          <input className='w-full rounded-full' type='text' placeholder='Enter user text here' ref={userInputRef} defaultValue='User'></input>
+          <input
+            className='w-full rounded-full'
+            ref={userInputRef}
+            type='text'
+            defaultValue='User'
+            placeholder='Enter user text here'
+          ></input>
           <div
             className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black'
-            onClick={() => {
-              if (!personaClient || !userInputRef.current.value) {
-                return
-              }
-
-              personaClient.sendUserText(userInputRef.current.value)
-            }}
+            onClick={onUser}
           >
             Submit
           </div>
           <div className='w-32'></div>
         </div>
         <div className='flex items-center w-full gap-4'>
-          <input className='w-full rounded-full' type='text' placeholder='Enter assistant text here' ref={assistantInputRef} defaultValue='Assistant'></input>
+          <input
+            className='w-full rounded-full'
+            ref={assistantInputRef}
+            type='text'
+            defaultValue='Assistant'
+            placeholder='Enter assistant text here'
+          ></input>
           <div
             className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black'
-            onClick={() => {
-              if (!personaClient || !assistantInputRef.current.value) {
-                return
-              }
-
-              personaClient.sayText(assistantInputRef.current.value)
-            }}
+            onClick={onAssistant}
           >
             Submit
           </div>
           <div className='w-32'></div>
         </div>
         <div className='flex items-center w-full gap-4'>
-          <input className='w-full rounded-full' type='text' placeholder='Enter initial message here' ref={initialInputRef} defaultValue='Initialization'></input>
+          <input
+            className='w-full rounded-full'
+            ref={initialInputRef}
+            type='text'
+            defaultValue='Initialization'
+            placeholder='Enter initial message here'
+          ></input>
           <div
             className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black'
-            onClick={async () => {
-              const initialMsg = initialInputRef.current.value
-              const selPersonaId = personaSelRef.current.value
-
-              if (!personaClient || !initialMsg || !selPersonaId) {
-                return
-              }
-
-              setInitialMsgState('Saving...')
-              const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/initialMessage?apikey=${API_KEY}`, { initialMessage: initialMsg });
-
-              if (res.status === 200) {
-                setInitialMsgState('Success')
-              } else {
-                setInitialMsgState('Error')
-              }
-            }}
+            onClick={onInitialization}
           >
             Submit
           </div>
           <div className='w-32'>{initialMsgState}</div>
         </div>
         <div className='flex items-center w-full gap-4'>
-          <input className='w-full rounded-full' type='text' placeholder='Enter rate limit message here' ref={rateLimitInputRef} defaultValue='Rate limit'></input>
+          <input
+            className='w-full rounded-full'
+            ref={rateLimitInputRef}
+            type='text'
+            defaultValue='Rate limit'
+            placeholder='Enter rate limit message here'
+          ></input>
           <div
             className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black'
-            onClick={async () => {
-              const rateLimitMsg = rateLimitInputRef.current.value
-              const selPersonaId = personaSelRef.current.value
-
-              if (!personaClient || !rateLimitMsg || !selPersonaId) {
-                return
-              }
-
-              setRateLimitMsgState('Saving...')
-              const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/rateLimitMessage?apikey=${API_KEY}`, { rateLimitMessage: rateLimitMsg });
-
-              if (res.status === 200) {
-                setRateLimitMsgState('Success')
-              } else {
-                setRateLimitMsgState('Error')
-              }
-            }}
+            onClick={onRateLimit}
           >
             Submit
           </div>
@@ -144,20 +210,42 @@ export default function VoiceChat() {
         </div>
       </div>
       <div className='flex items-center w-full gap-4'>
-        <select className='w-full rounded-full cursor-pointer' ref={personaSelRef}>
+        <select
+          className='w-full rounded-full cursor-pointer'
+          ref={personaSelRef}
+        >
           {personaArr.map((persona, index) => <option key={index} value={persona._id}>{persona.name}</option>)}
         </select>
         <div className='w-32'></div>
       </div>
       <div className='flex flex-col w-full gap-2'>
         <div className='text-lg '>For guidance on prompt engineering techniques for your Persona, see:</div>
-        <a className='text-blue-500 hover:text-blue-900 w-fit' href="https://www.promptingguide.ai/introduction" target="_blank" rel="noreferrer">Prompt Engineering Guide 1</a>
-        <a className='text-blue-500 hover:text-blue-900 w-fit' href="https://github.com/brexhq/prompt-engineering" target="_blank" rel="noreferrer">Prompt Engineering Guide 2</a>
+        <a
+          className='text-blue-500 hover:text-blue-900 w-fit'
+          href="https://www.promptingguide.ai/introduction"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Prompt Engineering Guide 1
+        </a>
+        <a
+          className='text-blue-500 hover:text-blue-900 w-fit'
+          href="https://github.com/brexhq/prompt-engineering"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Prompt Engineering Guide 2
+        </a>
       </div>
       <div className='flex w-full gap-4'>
         <div className='flex flex-col w-full gap-2'>
           <div className='text-lg'>Prompt</div>
-          <textarea rows={20} placeholder='Enter the prompt here' ref={promptTextRef} defaultValue='Prompt'></textarea>
+          <textarea
+            ref={promptTextRef}
+            rows={20}
+            defaultValue='Prompt'
+            placeholder='Enter the prompt here'
+          ></textarea>
           <div className='flex flex-col'>
             <div>Possible variables:</div>
             <div>- ***PERSONA_VOICE_SCHEMA***: required to make use of the Actions schema</div>
@@ -167,23 +255,7 @@ export default function VoiceChat() {
           <div className='flex items-center gap-4'>
             <div
               className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black w-fit'
-              onClick={async () => {
-                const promptText = promptTextRef.current.value
-                const selPersonaId = personaSelRef.current.value
-
-                if (!personaClient || !promptText || !selPersonaId) {
-                  return
-                }
-
-                setPromptState('Saving...')
-                const res = await axios.put(`https://app.sindarin.tech/api/personas/${selPersonaId}/prompt?apikey=${API_KEY}`, { prompt: promptText });
-
-                if (res.status === 200) {
-                  setPromptState('Success')
-                } else {
-                  setPromptState('Error')
-                }
-              }}
+              onClick={onPrompt}
             >
               Update prompt
             </div>
@@ -191,34 +263,30 @@ export default function VoiceChat() {
           </div>
           <div
             className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black w-fit'
-            onClick={async () => {
-              const selPersonaId = personaSelRef.current.value
-
-              if (!personaArr.length || !selPersonaId) {
-                return
-              }
-
-              const selPersonName = personaArr.find((persona) => persona._id === selPersonaId)?.name
-
-              if (!personaClient || !selPersonName) {
-                return
-              }
-
-              await personaClient.init('admin', selPersonName)
-            }}
+            onClick={onNewChat}
           >
             New chat (continuous)
           </div>
         </div>
         <div className='flex flex-col w-full gap-2'>
           <div className='text-lg'>Actions schema</div>
-          <textarea rows={20} placeholder='Enter the actions schema here' defaultValue='Actions schema' ref={actionsSchemaTextRef}></textarea>
+          <textarea
+            ref={actionsSchemaTextRef}
+            rows={20}
+            defaultValue='Actions schema'
+            placeholder='Enter the actions schema here'
+          ></textarea>
           <div className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black w-fit'>Update actions schema</div>
         </div>
       </div>
       <div className='flex flex-col w-full gap-2'>
         <div className='text-lg'>Current state</div>
-        <textarea rows={20} placeholder='' defaultValue='Current state' ref={currentStateTextRef}></textarea>
+        <textarea
+          ref={currentStateTextRef}
+          rows={20}
+          defaultValue='Current state'
+          placeholder=''
+        ></textarea>
         <div className='px-4 py-2 text-white bg-green-500 rounded-full cursor-pointer hover:text-black w-fit'>Update state</div>
         <div>Error</div>
       </div>

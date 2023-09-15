7db722ac5438d280d131724406f5db0f8a164d2d
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-09-15 08:30:09 -0500

    backend

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,7 @@
+export const IS_DEV_MODE = process.env.NODE_ENV === 'development'
+
 export const DEPLOY_URL = `https://vercel.com/new/clone?repository-url=https://github.com/sindarin-ventures/persona-webapp&project-name=precedent&repository-name=precedent&demo-title=Precedent&demo-description=An%20opinionated%20collection%20of%20components%2C%20hooks%2C%20and%20utilities%20for%20your%20Next%20project.&demo-url=https://precedent.dev&demo-image=https://precedent.dev/opengraph-image&env=GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET,NEXTAUTH_SECRET&envDescription=How%20to%20get%20these%20env%20variables:&envLink=https://github.com/steven-tey/precedent/blob/main/.env.example&stores=%5B%7B"type":"postgres"%7D%5D`;
+
 export const API_KEY = 'abc'
+
+export const BACKEND_URL = IS_DEV_MODE ? 'http://127.0.0.1:4000/main' : ''
---
+++ lib/mongo-db.ts
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import { BACKEND_URL } from './constants'
+
+
+export const saveData = async (data: any) => {
+  try {
+    if (!data) {
+      return
+    }
+
+    const saveUrl = data._id ? `${BACKEND_URL}/update/${data._id}` : `${BACKEND_URL}/add`
+    delete data._id
+    const res = await axios.post(saveUrl, data)
+    return res
+  } catch (e) {
+    console.log('mongo.db#saveData: e: ', e)
+  }
+}
+
+
+export const getData = async (id: string) => {
+  try {
+    if (!id) {
+      return
+    }
+
+    const getUrl = `${BACKEND_URL}/${id}`
+    const res = await axios.get(getUrl)
+    return res?.data
+  } catch (e) {
+    console.log('mongo.db#getData: e: ', e)
+  }
+}
+
+
+export const getAllData = async () => {
+  try {
+    const getAllUrl = `${BACKEND_URL}/`
+    const res = await axios.get(getAllUrl)
+    return res?.data
+  } catch (e) {
+    console.log('mongo.db#getAllData: e: ', e)
+  }
+}
+
+
+export const removeData = async (id: string) => {
+  try {
+    if (!id) {
+      return
+    }
+
+    const removeUrl = `${BACKEND_URL}/remove/${id}`
+    const res = await axios.post(removeUrl)
+    return res
+  } catch (e) {
+    console.log('mongo.db#removeData: e: ', e)
+  }
+}
+
+
+export const getEmailData = async (email: string) => {
+  try {
+    if (!email) {
+      return
+    }
+
+    const getUrl = `${BACKEND_URL}/getemaildata/${email}`
+    const res = await axios.get(getUrl)
+    return res?.data
+  } catch (e) {
+    console.log('mongo.db#getData: e: ', e)
+  }
+}

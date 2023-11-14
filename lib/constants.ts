993f5cd203b6c1157ec4b3cac0f69d99e75bdf9f
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-14 17:46:14 -0600

    update env

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,7 +17,7 @@ export const ADMIN_EMAIL = process.env.NEXT_PUBLIC_ADMIN_EMAIL
 export const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI
 export const DB_NAME = process.env.NEXT_PUBLIC_DB_NAME
 
-export const MASTER_API_KEY = process.env.NEXT_PUBLIC_MASTER_API_KEY
+export const MASTER_API_KEY = process.env.SINDARIN_MASTER_API_KEY
 
 console.log('constants: NODE_ENV: ', NODE_ENV)
 console.log('constants: DEPLOY_URL: ', DEPLOY_URL)

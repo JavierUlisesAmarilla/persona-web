624ce8ea58238e5bd53748aa8a4829916ac97b63
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-15 15:37:32 -0600

    fix api responser handling

--- app/api/persona/teams/new/route.tsx
+++ app/api/persona/teams/new/route.tsx
@@ -8,7 +8,8 @@ export const POST = async (request: Request) => {
     const postData = await request.json()
     const res = await axios.post(`https://api.sindarin.tech/api/teams/new?apikey=${MASTER_API_KEY}`, postData)
     const success = res.status === 200
-    const token = success ? res.data.substring(36) : ''
+    // const token = success ? res.data.substring(36) : ''
+    const token = success ? res.data.apiKey : ''
     return NextResponse.json({success, token})
   } catch (error: any) {
     return NextResponse.json({

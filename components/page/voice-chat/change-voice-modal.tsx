304b75e1df735e4a4e57ba00c2db77e7bbca994c
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-22 22:47:25 -0600

    start updating change voice modal

--- components/page/voice-chat/change-voice-modal.tsx
+++ components/page/voice-chat/change-voice-modal.tsx
@@ -1,26 +1,77 @@
 /* eslint-disable react/no-unescaped-entities */
 'use client'
+import {useEffect, useState} from 'react'
 import {BlueButton} from '@/components/shared/button'
 import {CommonModal} from '@/components/shared/common-modal'
 import {InputText} from '@/components/shared/input-text'
-import React from 'react'
 import {AiFillCustomerService, AiFillDelete, AiFillSound, AiOutlineEdit, AiOutlineShareAlt} from 'react-icons/ai'
 import {useZustand} from '../../../lib/store/use-zustand'
 import {UserSelect} from '../../shared/user-select'
-
+import {SINDARIN_API_URL} from '@/lib/constants'
+import {useApiKey} from '@/lib/hooks/use-api-key'
+interface VoiceOption {
+  gender: string;
+  age: string;
+  accent: string;
+}
 
 interface Props {
   show?: boolean
   onClose?: React.MouseEventHandler<SVGElement>
 }
 
-
-export const ChangeVoiceModal = ({
+export const ChangeVoiceModal: React.FC<Props> = ({
   show,
   onClose,
-}: Props) => {
+}) => {
   const {personaArr, selPersonaIndex} = useZustand()
   const selPersonaName = personaArr[selPersonaIndex]?.name
+  const [voices, setVoices] = useState<VoiceOption[]>([])
+  const [selectedGender, setSelectedGender] = useState<string>('')
+  const [selectedAge, setSelectedAge] = useState<string>('')
+  const [selectedAccent, setSelectedAccent] = useState<string>('')
+  
+  const apiKey = useApiKey()
+
+  useEffect(() => {
+    fetch(`${SINDARIN_API_URL}/api/voices?apikey=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        setVoices(data)
+      })
+      .catch(error => console.error('Error fetching voice options:', error))
+  }, [])
+
+  const getFilteredVoices = (gender?: string, age?: string, accent?: string) => {
+    return voices.filter(voice =>
+      (!gender || gender === '-' ? true : voice.gender === gender) &&
+      (!age || age === '-' ? true : voice.age === age) &&
+      (!accent || accent === '-' ? true : voice.accent === accent)
+    );
+  };
+
+  const uniqueGenders = Array.from(new Set(voices.map(voice => voice.gender)))
+    .map(gender => ({
+      gender,
+      count: getFilteredVoices(gender, selectedAge, selectedAccent).length
+    }))
+    .filter(gender => gender.count > 0);
+
+  const uniqueAges = Array.from(new Set(voices.map(voice => voice.age)))
+    .map(age => ({
+      age,
+      count: getFilteredVoices(selectedGender, age, selectedAccent).length
+    }))
+    .filter(age => age.count > 0);
+
+  const uniqueAccents = Array.from(new Set(voices.map(voice => voice.accent)))
+    .map(accent => ({
+      accent,
+      count: getFilteredVoices(selectedGender, selectedAge, accent).length
+    }))
+    .filter(accent => accent.count > 0);
+
+  const filteredVoices = getFilteredVoices(selectedGender, selectedAge, selectedAccent);
 
   return (
     <CommonModal
@@ -32,50 +83,42 @@ export const ChangeVoiceModal = ({
         <div className='flex items-center gap-3'>
           <div className='flex items-center gap-1'>
             <div className='text-red-500'>Gender:</div>
-            <UserSelect>
-              <option value='male'>male</option>
-              <option value='female'>female</option>
+            <UserSelect value={selectedGender} onChange={(e) => setSelectedGender(e.target.value)}>
+              <option value="-">-</option>
+              {uniqueGenders.map(({ gender, count }) => (
+                gender !== null ? <option key={gender} value={gender}>{`${gender} (${count})`}</option> : null
+              ))}
             </UserSelect>
           </div>
           <div className='flex items-center gap-1'>
             <div className='text-red-500'>Age:</div>
-            <UserSelect>
-              <option value='0'>{'<20'}</option>
-              <option value='1'>{'<30'}</option>
-              <option value='2'>{'<40'}</option>
+            <UserSelect value={selectedAge} onChange={(e) => setSelectedAge(e.target.value)}>
+              <option value="-">-</option>
+              {uniqueAges.map(({ age, count }) => (
+                age !== null ? <option key={age} value={age}>{`${age} (${count})`}</option> : null
+              ))}
             </UserSelect>
           </div>
           <div className='flex items-center gap-1'>
             <div className='text-red-500'>Accent:</div>
-            <UserSelect>
-              <option value='0'>{'<20'}</option>
-              <option value='1'>{'<30'}</option>
-              <option value='2'>{'<40'}</option>
+            <UserSelect value={selectedAccent} onChange={(e) => setSelectedAccent(e.target.value)}>
+              <option value="-">-</option>
+              {uniqueAccents.map(({ accent, count }) => (
+                accent !== null ? <option key={accent} value={accent}>{`${accent} (${count})`}</option> : null
+              ))}
             </UserSelect>
           </div>
         </div>
-        <div className='flex flex-col gap-3 p-6 border rounded-lg bg-bg-light border-border-gray'>
-          <div className='flex items-center justify-between gap-3'>
-            <div className='flex items-center gap-3 cursor-pointer'>
-              <div>Male, Middle Age, British</div>
-              <AiFillSound className='text-sm'/>
+        <div className='flex flex-col gap-3 p-6 border rounded-lg bg-bg-light border-border-gray' style={{ maxHeight: '300px', overflowY: 'auto' }}>
+          {filteredVoices.map((voice, index) => (
+            <div key={index} className='flex items-center justify-between gap-3'>
+              <div className='flex items-center gap-3 cursor-pointer'>
+                <div>{`${voice.gender}, ${voice.age}, ${voice.accent}`}</div>
+                <AiFillSound className='text-sm'/>
+              </div>
+              <BlueButton>Use Voice</BlueButton>
             </div>
-            <BlueButton>Use Voice</BlueButton>
-          </div>
-          <div className='flex items-center justify-between gap-3'>
-            <div className='flex items-center gap-3 cursor-pointer'>
-              <div>Male, Middle Age, British</div>
-              <AiFillSound className='text-sm'/>
-            </div>
-            <BlueButton>Use Voice</BlueButton>
-          </div>
-          <div className='flex items-center justify-between gap-3'>
-            <div className='flex items-center gap-3 cursor-pointer'>
-              <div>Male, Middle Age, British</div>
-              <AiFillSound className='text-sm'/>
-            </div>
-            <BlueButton>Use Voice</BlueButton>
-          </div>
+          ))}
         </div>
         <div className='flex flex-col gap-3 p-6 border rounded-lg bg-bg-light border-border-gray'>
           <div className='text-sm text-center text-red-500'>Import from ElevenLabs</div>
@@ -126,3 +169,4 @@ export const ChangeVoiceModal = ({
     </CommonModal>
   )
 }
+

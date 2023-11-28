0141aed292bd0b84cc1287669ed6d5c0e0188219
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-28 15:48:15 -0300

    debug

--- lib/hooks/use-api-key.ts
+++ lib/hooks/use-api-key.ts
@@ -3,6 +3,7 @@ import {useZustand} from '../store/use-zustand'
 
 export const useApiKey = () => {
   const {apiKeyArr, selApiKeyIndex} = useZustand()
+  console.log('useApiKey: apiKeyArr[selApiKeyIndex]: ', apiKeyArr[selApiKeyIndex], apiKeyArr, selApiKeyIndex)
   const apiKey = apiKeyArr[selApiKeyIndex]?.apiKey
   return apiKey
 }

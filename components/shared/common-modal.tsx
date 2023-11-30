27aa35ae7a5e03b6ed2050924e42ba8c592e9c00
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-30 13:11:56 -0600

    update blur and backdrop

--- components/shared/common-modal.tsx
+++ components/shared/common-modal.tsx
@@ -19,8 +19,8 @@ export const CommonModal = ({
   useOutsideAlerter(divRef, onClose)
 
   return (
-    <div className={`fixed z-50 flex items-center justify-center w-screen h-screen overflow-hidden -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-bg-dark bg-opacity-70 backdrop-blur ${!show && 'hidden'}`}>
-      <div ref={divRef} className={`relative overflow-auto border-2 rounded-lg border-border-gray bg-bg-gray max-w-[90%] max-h-[90%] ${className}`}>
+    <div className={`fixed z-50 flex items-center justify-center w-screen h-screen overflow-hidden -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-bg-dark bg-opacity-10 backdrop-blur-sm ${!show && 'hidden'}`}>
+      <div ref={divRef} className={`relative overflow-auto rounded-lg border-border-gray bg-bg-gray max-w-[90%] max-h-[90%] ${className}`}>
         {/* Close */}
         {/* {onClose &&
           <AiFillCloseCircle

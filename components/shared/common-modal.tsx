70c31d6dfeb9598aaa3d340f08e6f49c567cccbf
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-11-30 16:28:09 -0300

    fix blur issue

--- components/shared/common-modal.tsx
+++ components/shared/common-modal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import {ReactNode, useRef} from 'react'
 
 import {useOutsideAlerter} from '@/lib/hooks/use-outside-alerter'
@@ -19,7 +18,8 @@ export const CommonModal = ({
   useOutsideAlerter(divRef, onClose)
 
   return (
-    <div className={`fixed z-50 flex items-center justify-center w-[calc(100vw+3px)] h-[calc(100vh+3px)] overflow-hidden -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-bg-dark bg-opacity-70 backdrop-blur ${!show && 'hidden'}`}>
+    <div className={`fixed z-50 flex items-center justify-center w-[calc(100vw+3px)] h-[calc(100vh+3px)] overflow-hidden -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ${!show && 'hidden'}`}>
+      <div className='absolute top-0 left-0 w-full h-full bg-bg-dark bg-opacity-70 backdrop-blur'/>
       <div ref={divRef} className={`relative overflow-auto border-2 rounded-lg border-border-gray bg-bg-gray max-w-[90%] max-h-[90%] ${className}`}>
         {/* Close */}
         {/* {onClose &&

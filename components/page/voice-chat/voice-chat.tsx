550f9499b06e64ec7ae9b2f526a6bf3146de1b9e
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-12-06 19:37:27 -0600

    updates for message limit

--- components/page/voice-chat/voice-chat.tsx
+++ components/page/voice-chat/voice-chat.tsx
@@ -77,6 +77,8 @@ export const VoiceChat = () => {
 
   const [currentConversationMessages, setCurrentConversationMessages] = useState<MessageDetail[]>([])
   const [currentConversationId, setCurrentConversationId] = useState('')
+  const [rateLimitCheckboxState, setRateLimitCheckboxState] = useState(false)
+  const [rateLimit, setRateLimit] = useState(200)
 
   const apiKey = useApiKey()
 
@@ -145,10 +147,26 @@ export const VoiceChat = () => {
       }
 
       setRateLimitMsgState('Saving...')
-      const res = await axios.put(`${SINDARIN_API_URL}/api/personas/${selPersonaId}/rateLimitMessage?apikey=${apiKey}`, {rateLimitMessage: personaArr[selPersonaIndex].rateLimitMessage})
+      const res = await axios.put(`${SINDARIN_API_URL}/api/personas/${selPersonaId}/rateLimitMessage?apikey=${apiKey}`, {rateLimitMessage: personaArr[selPersonaIndex].rateLimitMessage, rateLimit})
       setRateLimitMsgState(res.status === 200 ? 'Success' : 'Error')
     } catch (error) {
       console.log('VoiceChat#onRateLimit: error: ', error)
+    }
+  }
+
+  const onToggleRateLimit = async (enabled: boolean) => {
+    try {
+      const selPersonaId = personaArr[selPersonaIndex]?._id
+
+      if (!selPersonaId) {
+        return
+      }
+
+      setRateLimitMsgState('Saving...')
+      const res = await axios.put(`${SINDARIN_API_URL}/api/personas/${selPersonaId}/toggleRateLimit?apikey=${apiKey}`, {enabled})
+      setRateLimitMsgState(res.status === 200 ? 'Success' : 'Error')
+    } catch (error) {
+      console.log('VoiceChat#onToggleRateLimit: error: ', error)
     }
   }
 
@@ -313,7 +331,7 @@ export const VoiceChat = () => {
               </div>
             </div>
             <div className='flex items-center justify-between w-full gap-6'>
-              <div className='flex-grow gap-2 p-4 border rounded-lg border-border-dark-blue bg-bg-dark-blue'>
+              <div className='flex-grow gap-2 p-4 border rounded-lg border-border-dark-blue bg-bg-dark-blue' style={{width: '50%'}}>
                 <div className='flex items-end justify-between'>
                   <div className='text-xs text-gray-400'>Initial Message</div>
                   <BlueButton onClick={onInitialization}>Save</BlueButton>
@@ -326,17 +344,56 @@ export const VoiceChat = () => {
                   onChange={(e) => setScenarioInitMsg(selPersonaIndex, e.target.value)}
                 />
               </div>
-              <div className='flex-grow gap-2 p-4 border rounded-lg border-border-dark-blue bg-bg-dark-blue'>
-                <div className='flex items-end justify-between'>
-                  <div className='text-xs text-gray-400'>Rate Limit Message</div>
-                  <BlueButton onClick={onRateLimit}>Save</BlueButton>
+              <div className='flex-grow gap-2 p-4 border rounded-lg border-border-dark-blue bg-bg-dark-blue' style={{width: '50%'}}>
+                <div className='flex items-center justify-between'>
+                  <div className='flex items-center'>
+                    <div className='text-xs text-gray-400'>Message Limit</div>
+                    <label className="flex items-center cursor-pointer">
+                      <div className="relative">
+                        <input type="checkbox" className="sr-only" checked={rateLimitCheckboxState} onChange={(e) => {
+                          setRateLimitCheckboxState(e.target.checked)
+                          onToggleRateLimit(e.target.checked)
+                        }}
+                        />
+                        <div className={`block bg-bg-dark-blue w-4 h-4 ml-2 rounded border border-gray-500 ${rateLimitCheckboxState ? '' : 'bg-bg-dark-blue'}`}>
+                          <svg className={`text-white w-4 h-4 pointer-events-none ${rateLimitCheckboxState ? '' : 'hidden'}`} viewBox="0 0 20 20" fill="currentColor" style={{position: 'relative', left: '-1px', bottom: '1px'}}>
+                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"/>
+                          </svg>
+                        </div>
+                      </div>
+                    </label>
+                    {rateLimitCheckboxState &&
+                      <input
+                        type="text"
+                        // className={`rounded ${rateLimit.toString().length == 1 ? 'w-3' : rateLimit.toString().length == 2 ? 'w-5' : 'w-7'} h-4 bg-bg-dark-blue border-b border-gray-500 text-white text-xs ml-2 pr-0 pl-0`}
+                        className={`rounded ${rateLimit.toString().length === 1 ? 'w-4' : rateLimit.toString().length === 2 ? 'w-6' : 'w-8'} h-4 bg-bg-dark-blue border-b border-gray-500 text-white text-xs ml-2 pr-1 pl-1`}
+                        value={rateLimit.toString()}
+                        maxLength={3}
+                        placeholder="Lim"
+                        onKeyPress={(event) => {
+                          if (!/[0-9]/.test(event.key)) {
+                            return event.preventDefault()
+                          }
+                        }}
+                        onChange={(event) => {
+                          setRateLimit(parseInt(event.target.value) || 0)
+                        }}
+                      />
+                    }
+                  </div>
+                  <BlueButton onClick={onRateLimit} disabled={!rateLimitCheckboxState}>Save</BlueButton>
                 </div>
                 <InputText
-                  classNames='w-full bg-transparent text-white border-0'
+                  classNames={`w-full bg-transparent ${rateLimitCheckboxState ? 'text-white' : 'text-gray-400'} border-0`}
                   style={{padding: 0}}
                   value={personaArr[selPersonaIndex]?.rateLimitMessage || ''}
                   placeholder='Enter rate limit message here'
-                  onChange={(e) => setScenarioRateLimit(selPersonaIndex, e.target.value)}
+                  onChange={(e) => {
+                    if (rateLimitCheckboxState) {
+                      setScenarioRateLimit(selPersonaIndex, e.target.value)
+                    }
+                  }}
+                  disabled={!rateLimitCheckboxState}
                 />
               </div>
             </div>

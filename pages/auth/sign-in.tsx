69deff928f75fb989fbc618d262b0c471babab1a
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-12-09 16:04:45 -0300

    sign in style

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -1,8 +1,12 @@
+import 'tailwindcss/tailwind.css'
+
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable require-jsdoc */
 /* eslint-disable jsdoc/require-jsdoc */
 import {getProviders, signIn} from 'next-auth/react'
+import {AiFillGoogleCircle} from 'react-icons/ai'
+import {SiAuth0} from 'react-icons/si'
 
 
 export async function getServerSideProps() {
@@ -17,18 +21,22 @@ export async function getServerSideProps() {
 
 export default function SignIn({providers}: {providers: any}) {
   return (
-    <div style={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%', border: '1px solid red'}}>
-      <div>Login to Sindarin</div>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        {Object.keys(providers).map((providerId, providerIndex) =>
-          <div
-            key={providerIndex}
-            style={{cursor: 'pointer'}}
-            onClick={() => signIn(providerId)}
-          >
-            {providers[providerId].name}
-          </div>,
-        )}
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <div className='flex flex-col gap-6 p-16 border rounded-lg bg-bg-gray border-border-gray'>
+        <div className='text-2xl font-semibold'>Login to Sindarin</div>
+        <div className='flex flex-col gap-3'>
+          {Object.keys(providers).map((providerId, providerIndex) =>
+            <div
+              key={providerIndex}
+              className='flex items-center gap-3 p-3 border rounded-lg cursor-pointer bg-bg-light border-border-gray hover:text-text-blue'
+              onClick={() => signIn(providerId)}
+            >
+              {providerId === 'auth0' && <SiAuth0 className='text-lg'/>}
+              {providerId === 'google' && <AiFillGoogleCircle className='text-lg'/>}
+              <div className='font-medium'>Login with {providers[providerId].name}</div>
+            </div>,
+          )}
+        </div>
       </div>
     </div>
   )

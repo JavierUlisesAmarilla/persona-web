cd6d063c121ab03ff15c4e53d9c15be4cdd98ad2
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-12-09 16:51:41 -0600

    fix login

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -1,15 +1,34 @@
 import 'tailwindcss/tailwind.css'
 
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next'
+
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable require-jsdoc */
 /* eslint-disable jsdoc/require-jsdoc */
 import {getProviders, signIn} from 'next-auth/react'
+// import {authOptions} from '../../app/api/auth/[...nextauth]'
+import {authOptions} from '../../app/api/auth/[...nextauth]/route'
+import {getServerSession} from 'next-auth/next'
 import {AiFillGoogleCircle} from 'react-icons/ai'
 import {SiAuth0} from 'react-icons/si'
 
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        // permanent: false,
+      },
+    }
+  }
+
   const providers = await getProviders()
   return {
     props: {
@@ -20,6 +39,7 @@ export async function getServerSideProps() {
 
 
 export default function SignIn({providers}: {providers: any}) {
+  console.log('SignIn#render: providers: ', providers)
   return (
     <div className='flex items-center justify-center w-screen h-screen'>
       <div className='flex flex-col gap-6 p-16 border rounded-lg bg-bg-gray border-border-gray'>
